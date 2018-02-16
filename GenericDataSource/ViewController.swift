393e860e3059c5b4820f8b040da58c831f63b55d
Helena Leitão <hmleit1@gmail.com>
2018-02-16 14:24:32 -0200

    Initial Commit

---
+++ GenericDataSource/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GenericDataSource
+//
+//  Created by Helena Leitão on 16/02/2018.
+//  Copyright © 2018 Teste. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
